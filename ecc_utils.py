681b9fcd44a3d88e26bdb91fb1f728a18a0b1e29
John Dong <pls@gooby.co>
2021-12-13 16:33:14 -0500

    Cleaned up XDK support code

--- ecc_utils.py
+++ ecc_utils.py
@@ -2,9 +2,9 @@
 
 from enum import Enum
 from io import BytesIO
-from struct import pack, unpack
-from argparse import ArgumentParser
 from typing import Union
+from argparse import ArgumentParser
+from struct import pack, unpack_from
 
 class BLOCK_TYPE(Enum):
 	SMALL = 0x0
@@ -14,20 +14,20 @@ class BLOCK_TYPE(Enum):
 
 def get_block_type(page_20_spare_data: Union[bytes, bytearray]) -> BLOCK_TYPE:
 	if page_20_spare_data[0] == 0xFF:
-		print("Detected 256/512MB Big Block Flash")
+		# print("Detected 256/512MB Big Block Flash")
 		return BLOCK_TYPE.BIG
 	elif page_20_spare_data[5] == 0xFF:
 		if page_20_spare_data[:2] == b"\x01\x00":
-			print("Detected 16/64MB Small Block Flash")
+			# print("Detected 16/64MB Small Block Flash")
 			return BLOCK_TYPE.SMALL
 		elif page_20_spare_data[:2] == b"\x00\x01":
-			print("Detected 16/64MB Big on Small Flash")
+			# 3print("Detected 16/64MB Big on Small Flash")
 			return BLOCK_TYPE.BIG_ON_SMALL
 		else:
-			print("Can't detect flash type")
+			# print("Can't detect flash type")
 			return BLOCK_TYPE.UNKNOWN
 	else:
-		print("Can't detect flash type")
+		# print("Can't detect flash type")
 		return BLOCK_TYPE.UNKNOWN
 
 def calcecc(data: Union[bytes, bytearray]) -> bytes:
@@ -35,33 +35,32 @@ def calcecc(data: Union[bytes, bytearray]) -> bytes:
 	val = 0
 	for i in range(0x1066):
 		if not i & 31:
-			v = ~unpack("<L", data[i // 8:(i // 8) + 4])[0]
+			v = ~unpack_from("<I", data, i // 8)[0]
 		val ^= v & 1
 		v >>= 1
 		if val & 1:
 			val ^= 0x6954559
 		val >>= 1
 	val = ~val
-	return data[:-4] + pack("<L", ((val << 6) & 0xFFFFFFFF) ^ (data[-4] & 0x3F))
+	return data[:-4] + pack("<I", ((val << 6) & 0xFFFFFFFF) ^ (data[-4] & 0x3F))
 
-def addecc(data: Union[bytes, bytearray], block: int = 0, off_8: Union[bytes, bytearray] = b"\x00" * 4, block_type: BLOCK_TYPE = BLOCK_TYPE.BIG_ON_SMALL):
-	res = b""
-	while len(data):
-		d = (data[:0x200] + b"\x00" * 0x200)[:0x200]
-		data = data[0x200:]
+def addecc(data: Union[bytes, bytearray], block: int = 0, block_type: BLOCK_TYPE = BLOCK_TYPE.BIG_ON_SMALL):
+	with BytesIO(data) as rbio, BytesIO() as wbio:
+		while rbio.tell() < len(data):
+			d = rbio.read(0x200)
 
-		if block_type == BLOCK_TYPE.BIG_ON_SMALL:
-			d += pack("<BL3B4s4s", 0, block // 32, 0xFF, 0, 0, off_8, b"\x00" * 4)
-		elif block_type == BLOCK_TYPE.BIG:
-			d += pack("<BL3B4s4s", 0xFF, block // 256, 0, 0, 0, off_8, b"\x00" * 4)
-		elif block_type == BLOCK_TYPE.SMALL:
-			d += pack("<L4B4s4s", block // 32, 0, 0xFF, 0, 0, off_8, b"\x00" * 4)
-		else:
-			raise ValueError("Block type not supported")
-		d = calcecc(d)
-		block += 1
-		res += d
-	return res
+			if block_type == BLOCK_TYPE.BIG_ON_SMALL:
+				d += pack("<BI3B8x", 0, block // 32, 0xFF, 0, 0)
+			elif block_type == BLOCK_TYPE.BIG:
+				d += pack("<BI3B8x", 0xFF, block // 256, 0, 0, 0)
+			elif block_type == BLOCK_TYPE.SMALL:
+				d += pack("<I4B8x", block // 32, 0, 0xFF, 0, 0)
+			else:
+				raise ValueError("Block type not supported")
+			d = calcecc(d)
+			block += 1
+			wbio.write(d)
+		return wbio.getvalue()
 
 def unecc(image: Union[bytes, bytearray]) -> bytes:
 	with BytesIO(image) as rbio, BytesIO() as wbio:
@@ -75,59 +74,59 @@ def verify(data: Union[bytes, bytearray], page: int = 0, off_8: Union[bytes, byt
 	if block_type == BLOCK_TYPE.UNKNOWN:
 		print("aborting...")
 		return
-	while len(data):
-		d = (data[:0x200] + b"\x00" * 0x200)[:0x200]
-		file_ecc = data[0x200:0x210]
-		if block_type == BLOCK_TYPE.BIG_ON_SMALL:
-			block_num = page // 32
-			fs_seq_0 = file_ecc[0]
-			fs_seq_1 = file_ecc[3]
-			fs_seq_2 = file_ecc[4]
-			fs_seq_3 = file_ecc[6]
-			fs_size_1 = file_ecc[7]
-			fs_size_0 = file_ecc[8]
-			fs_page_count = file_ecc[9]
-			fs_unused_1 = b"\x00\x00"
-			fs_block_type = file_ecc[12] & 0x3f
-			ecc_2_1_0 = b"\x00\x00\x00"
-			d += pack("<BH7B2sB3s", fs_seq_0, block_num, fs_seq_1, fs_seq_2, 0xFF, fs_seq_3, fs_size_1, fs_size_0, fs_page_count, fs_unused_1, fs_block_type, ecc_2_1_0)
-		elif block_type == BLOCK_TYPE.BIG:
-			block_num = page // 256
-			fs_seq_2 = file_ecc[3]
-			fs_seq_1 = file_ecc[4]
-			fs_seq_0 = file_ecc[5]
-			fs_unused_1 = 0x00
-			fs_size_1 = file_ecc[7]
-			fs_size_0 = file_ecc[8]
-			fs_page_count = file_ecc[9]
-			fs_unused_2 = b"\x00\x00"
-			fs_block_type = file_ecc[12] & 0x3f
-			ecc_2_1_0 = b"\x00\x00\x00"
-			d += pack("<BH7B2sB3s", 0xFF, block_num, fs_seq_2, fs_seq_1, fs_seq_0, fs_unused_1, fs_size_1, fs_size_0, fs_page_count, fs_unused_2, fs_block_type, ecc_2_1_0)
-		elif block_type == BLOCK_TYPE.SMALL:
-			block_num = page // 32
-			fs_seq_0 = file_ecc[2]
-			fs_seq_1 = file_ecc[3]
-			fs_seq_2 = file_ecc[4]
-			fs_seq_3 = file_ecc[6]
-			fs_size_1 = file_ecc[7]
-			fs_size_0 = file_ecc[8]
-			fs_page_count = file_ecc[9]
-			fs_unused_1 = b"\x00\x00"
-			fs_block_type = file_ecc[12] & 0x3f
-			ecc_2_1_0 = b"\x00\x00\x00"
-			d += pack("<H8B2sB3s", block_num, fs_seq_0, fs_seq_1, fs_seq_2, 0xFF, fs_seq_3, fs_size_1, fs_size_0, fs_page_count, fs_unused_1, fs_block_type, ecc_2_1_0)
-		else:
-			raise ValueError("Block type not supported")
-		d = calcecc(d)
-		calc_ecc = d[0x200:]
-		if file_ecc[:12] == b"\x00" * 12:
-			print(f"Page 0x{page:02X} is bad")
-		elif calc_ecc != file_ecc and file_ecc[:12] != b"\xff" * 12:
-			print(f"ECC mismatch on page 0x{page:02X} (0x{(page + 1) * 0x210 - 0x10:02X})")
-			print(f"File: {file_ecc.hex()} Calculated: {calc_ecc.hex()}")
-		page += 1
-		data = data[0x210:]
+	with BytesIO(data) as rbio:
+		while rbio.tell() < len(data):
+			d = rbio.read(0x200)
+			file_ecc = rbio.read(0x10)
+			if block_type == BLOCK_TYPE.BIG_ON_SMALL:
+				block_num = page // 32
+				fs_seq_0 = file_ecc[0]
+				fs_seq_1 = file_ecc[3]
+				fs_seq_2 = file_ecc[4]
+				fs_seq_3 = file_ecc[6]
+				fs_size_1 = file_ecc[7]
+				fs_size_0 = file_ecc[8]
+				fs_page_count = file_ecc[9]
+				fs_unused_1 = b"\x00\x00"
+				fs_block_type = file_ecc[12] & 0x3f
+				ecc_2_1_0 = b"\x00\x00\x00"
+				d += pack("<BH7B2sB3s", fs_seq_0, block_num, fs_seq_1, fs_seq_2, 0xFF, fs_seq_3, fs_size_1, fs_size_0, fs_page_count, fs_unused_1, fs_block_type, ecc_2_1_0)
+			elif block_type == BLOCK_TYPE.BIG:
+				block_num = page // 256
+				fs_seq_2 = file_ecc[3]
+				fs_seq_1 = file_ecc[4]
+				fs_seq_0 = file_ecc[5]
+				fs_unused_1 = 0x00
+				fs_size_1 = file_ecc[7]
+				fs_size_0 = file_ecc[8]
+				fs_page_count = file_ecc[9]
+				fs_unused_2 = b"\x00\x00"
+				fs_block_type = file_ecc[12] & 0x3f
+				ecc_2_1_0 = b"\x00\x00\x00"
+				d += pack("<BH7B2sB3s", 0xFF, block_num, fs_seq_2, fs_seq_1, fs_seq_0, fs_unused_1, fs_size_1, fs_size_0, fs_page_count, fs_unused_2, fs_block_type, ecc_2_1_0)
+			elif block_type == BLOCK_TYPE.SMALL:
+				block_num = page // 32
+				fs_seq_0 = file_ecc[2]
+				fs_seq_1 = file_ecc[3]
+				fs_seq_2 = file_ecc[4]
+				fs_seq_3 = file_ecc[6]
+				fs_size_1 = file_ecc[7]
+				fs_size_0 = file_ecc[8]
+				fs_page_count = file_ecc[9]
+				fs_unused_1 = b"\x00\x00"
+				fs_block_type = file_ecc[12] & 0x3f
+				ecc_2_1_0 = b"\x00\x00\x00"
+				d += pack("<H8B2sB3s", block_num, fs_seq_0, fs_seq_1, fs_seq_2, 0xFF, fs_seq_3, fs_size_1, fs_size_0, fs_page_count, fs_unused_1, fs_block_type, ecc_2_1_0)
+			else:
+				raise ValueError("Block type not supported")
+			d = calcecc(d)
+			calc_ecc = d[0x200:]
+			if file_ecc[:12] == b"\x00" * 12:
+				print(f"Page 0x{page:02X} is bad")
+			elif calc_ecc != file_ecc and file_ecc[:12] != b"\xff" * 12:
+				print(f"ECC mismatch on page 0x{page:02X} (0x{(page + 1) * 0x210 - 0x10:02X})")
+				print(f"File: {file_ecc.hex()} Calculated: {calc_ecc.hex()}")
+			page += 1
 
 def main() -> None:
 	parser = ArgumentParser(description="", add_help=False)
